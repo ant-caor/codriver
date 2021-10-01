30d1d3cc558fc2d0e4e970ace2fd8f1bf48e7d5a
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 01:15:59 +0200

    Add React Navigation libraries.

--- android/app/src/main/java/com/codriver/MainActivity.java
+++ android/app/src/main/java/com/codriver/MainActivity.java
@@ -1,8 +1,17 @@
 package com.codriver;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
+  /**
+   * We override onCreate method to discard any Activity state persisted during Activity restart process.
+   * Please find more information at: https://github.com/software-mansion/react-native-screens#android
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
