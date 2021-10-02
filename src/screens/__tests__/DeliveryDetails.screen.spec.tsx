a0f6d7b849935d8c8162d65a852638ab0d93f5b6
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 20:10:03 +0200

    Update DeliveryDetails.screen.spec.tsx

--- src/screens/__tests__/DeliveryDetails.screen.spec.tsx
+++ src/screens/__tests__/DeliveryDetails.screen.spec.tsx
@@ -3,10 +3,15 @@ import {render} from '@testing-library/react-native';
 
 import {TestIds} from '../../res/constants';
 import {DeliveryDetails} from '..';
+import {RecoilRoot} from 'recoil';
 
 describe('Delivery Details Screen', () => {
   it('Shows deliveries details title correctly.', () => {
-    const {getByTestId} = render(<DeliveryDetails />);
+    const {getByTestId} = render(
+      <RecoilRoot>
+        <DeliveryDetails />
+      </RecoilRoot>,
+    );
     expect(
       getByTestId(TestIds.DeliveryDetails.Title).children[0].valueOf(),
     ).toEqual('Delivery details');
