1e301ede122f6a18ffca1a935fc7683d12bda120
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 02:53:26 +0200

    Update Home.tsx

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import * as Res from '../res';
 
@@ -10,12 +10,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     padding: Res.Constants.Dimensions.SCREEN_PADDING,
   },
+  section: {
+    marginBottom: Res.Constants.Dimensions.SPACE_BETWEEN_SECTIONS,
+  },
+  sectionTitle: {
+    fontSize: Res.Constants.Dimensions.TITLE_FONT_SIZE,
+    fontWeight: 'bold',
+  },
 });
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home Screen</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Deliveries</Text>
+      </View>
     </SafeAreaView>
   );
 };
