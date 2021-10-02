9b7496732035cdb004970778a280c966d5fd9451
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 20:10:00 +0200

    Update Home.screen.spec.tsx

--- src/screens/__tests__/Home.screen.spec.tsx
+++ src/screens/__tests__/Home.screen.spec.tsx
@@ -3,10 +3,15 @@ import {render} from '@testing-library/react-native';
 
 import {TestIds} from '../../res/constants';
 import {Home} from '..';
+import {RecoilRoot} from 'recoil';
 
 describe('Home Screen', () => {
   it('Shows deliveries section title correctly.', () => {
-    const {getByTestId} = render(<Home />);
+    const {getByTestId} = render(
+      <RecoilRoot>
+        <Home />
+      </RecoilRoot>,
+    );
     expect(
       getByTestId(TestIds.Home.DeliveriesSectionTitle).children[0].valueOf(),
     ).toEqual('Deliveries');
