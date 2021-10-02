efd0c2f46291919fb922eaa8ef21bbede186209b
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 23:18:00 +0200

    Update index.ts

--- src/res/constants/index.ts
+++ src/res/constants/index.ts
@@ -1,5 +1,6 @@
+import Colors from './colors';
 import Dimensions from './dimensions';
 import Routes from './routes';
 import TestIds from './testIds';
 
-export {Dimensions, TestIds, Routes};
+export {Dimensions, TestIds, Routes, Colors};
