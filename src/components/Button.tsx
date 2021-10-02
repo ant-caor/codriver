a13dcbea39ab9a1ef4699cb24b4ac34d69ef95b3
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 21:17:36 +0200

    Add Button component.

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+import * as Res from '../res';
+
+type ButtonProps = {
+  label: string;
+  handlePress: () => void;
+  marginTop?: number;
+};
+
+const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: '#0023',
+      paddingHorizontal: Res.Constants.Dimensions.BUTTON_HORIZONTAL_PADDING,
+      paddingVertical: Res.Constants.Dimensions.BUTTON_VERTICAL_PADDING,
+      borderRadius: Res.Constants.Dimensions.BUTTON_RADIUS,
+      marginTop: props.marginTop,
+    },
+    elevation: {
+      shadowColor: '#000',
+      shadowOffset: {
+        width: Res.Constants.Dimensions.SHADOW_OFFSET_WIDTH,
+        height: Res.Constants.Dimensions.SHADOW_OFFSET_HEIGHT,
+      },
+      shadowOpacity: Res.Constants.Dimensions.SHADOW_OPACITY,
+      shadowRadius: Res.Constants.Dimensions.SHADOW_RADIUS,
+    },
+  });
+
+  return (
+    <TouchableOpacity style={[styles.button, styles.elevation]}>
+      <Text>{props.label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+import Button from './Button';
 import Delivery from './Delivery';
 import Item from './Item';
 import List from './List';
 import Screen from './Screen';
 import Section from './Section';
 
-export {Screen, Item, Section, Delivery, List};
+export {Screen, Item, Button, Section, Delivery, List};
