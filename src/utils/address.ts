a6be4b005cc34aa8029c62e1542946aa7fcf93d9
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 18:23:59 +0200

    Update address.ts

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -1,7 +1,17 @@
+import {stringIsEmpty} from '.';
 import * as State from '../state';
 
 const formatDeliveryAddress = (delivery: State.Models.Delivery): string => {
-  return `${delivery.address}, ${delivery.city}.`;
+  const addressExists = !stringIsEmpty(delivery.address);
+  const cityExists = !stringIsEmpty(delivery.city);
+  if (addressExists && cityExists) {
+    return `${delivery.address}, ${delivery.city}.`;
+  } else if (addressExists) {
+    return `${delivery.address}.`;
+  } else if (cityExists) {
+    return `${delivery.city}.`;
+  }
+  return 'Unknown address.';
 };
 
 export {formatDeliveryAddress};
