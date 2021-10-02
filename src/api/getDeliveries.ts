a675c2732ca4fde2a4306ffdf4a42da9d35cc704
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 10:18:13 +0200

    Update getDeliveries.ts

--- src/api/getDeliveries.ts
+++ src/api/getDeliveries.ts
@@ -1,7 +1,5 @@
 const getDeliveries = () => {
-  return fetch('https://60e84194673e350017c21844.mockapi.io/api/deliveries', {
-    method: 'GET',
-  });
+  return fetch('https://60e84194673e350017c21844.mockapi.io/api/deliveries');
 };
 
 export default getDeliveries;
