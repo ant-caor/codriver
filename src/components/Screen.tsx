015f2953f528a82554d9d09d7b36a17c76e3a31e
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 14:10:24 +0200

    Update Screen.tsx
    Add optional back button.

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,11 +1,13 @@
+import {useNavigation} from '@react-navigation/native';
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import * as Res from '../res';
 
 type ScreenProps = {
   children: React.ReactNode;
+  showBackButton?: boolean;
 };
 
 const styles = StyleSheet.create({
@@ -14,10 +16,28 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     padding: Res.Constants.Dimensions.SCREEN_PADDING,
   },
+  backButton: {
+    marginBottom: Res.Constants.Dimensions.ITEM_PADDING,
+  },
 });
 
 const Screen: React.FunctionComponent<ScreenProps> = (props: ScreenProps) => {
-  return <SafeAreaView style={styles.container}>{props.children}</SafeAreaView>;
+  const navigation = useNavigation();
+
+  const handlePressBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {props.showBackButton === true && (
+        <TouchableOpacity style={styles.backButton} onPress={handlePressBack}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+      )}
+      {props.children}
+    </SafeAreaView>
+  );
 };
 
 export default Screen;
