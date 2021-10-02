4bf76f2d94c2424196837f73a616928aa73b8d4c
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-02 23:18:18 +0200

    Update Delivery.tsx

--- src/components/Delivery.tsx
+++ src/components/Delivery.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 import * as State from '../state';
 import * as Components from '../components';
 import * as Res from '../res';
 import * as Utils from '../utils';
+import {useRecoilState} from 'recoil';
 
 type DeliveryProps = {
   delivery: State.Models.Delivery;
@@ -17,11 +18,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   deliveryTextContent: {},
+  activeLabel: {
+    backgroundColor: '#77DD77',
+    width: 110,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingVertical: 6,
+    marginTop: 16,
+    borderRadius: Res.Constants.Dimensions.BUTTON_RADIUS,
+  },
 });
 
 const Delivery: React.FunctionComponent<DeliveryProps> = (
   props: DeliveryProps,
 ) => {
+  const [activeDeliveryId] = useRecoilState(State.Atoms.activeDeliveryIdState);
+
   const handleTouchOnDelivery = () => {
     if (props.handleTouchOnDelivery !== undefined) {
       props.handleTouchOnDelivery();
@@ -45,6 +58,11 @@ const Delivery: React.FunctionComponent<DeliveryProps> = (
         style={styles.deliveryTextContent}>
         {Utils.formatDeliveryAddress(props.delivery)}
       </Text>
+      {activeDeliveryId === props.delivery.id && (
+        <View style={styles.activeLabel}>
+          <Text>Active</Text>
+        </View>
+      )}
     </Components.Item>
   );
 };
