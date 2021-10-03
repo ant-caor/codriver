12557d611c53e179f03fe9e8e0787168042c4946
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-03 02:30:19 +0200

    Update List.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {FlatList, ListRenderItem, StyleSheet} from 'react-native';
+import {VirtualizedList, ListRenderItem, StyleSheet} from 'react-native';
 
 import * as Res from '../res';
 
@@ -21,13 +21,24 @@ const styles = StyleSheet.create({
 });
 
 const List = <T extends object>(props: ListProps<T>) => {
+  const getItemCount = (data: T[]) => {
+    return data?.length ? data?.length : 0;
+  };
+
+  const getItem = (data: T[], index: number) => {
+    return data[index];
+  };
+
   return (
-    <FlatList
+    <VirtualizedList
       style={styles.list}
       contentContainerStyle={styles.listContent}
       data={props.data}
       renderItem={props.renderItem}
       ListHeaderComponent={props.header}
+      getItemCount={getItemCount}
+      getItem={getItem}
+      initialNumToRender={10}
     />
   );
 };
