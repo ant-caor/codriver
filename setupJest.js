68a740c4d9f0f8f00e22f76ce1504c3e72bfa188
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-04 00:37:57 +0200

    Update setupJest.js

--- setupJest.js
+++ setupJest.js
@@ -1,3 +1,6 @@
 require('jest-fetch-mock').enableMocks();
 
 jest.doMock('recoil', () => require('recoil/native/recoil'));
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
