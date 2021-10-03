0eed230b16036cfd0b6cf66d3da045afc670e03c
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-03 21:45:41 +0200

    Update deliveries.ts

--- src/state/atoms/deliveries.ts
+++ src/state/atoms/deliveries.ts
@@ -1,9 +1,11 @@
 import {atom} from 'recoil';
 import {Models} from '..';
+import asyncStorageEffect from './effects/asyncStorageEffect';
 
 const deliveriesState = atom({
   key: 'deliveries',
   default: [] as Models.Delivery[],
+  effects_UNSTABLE: [asyncStorageEffect('deliveries')],
 });
 
 export default deliveriesState;
