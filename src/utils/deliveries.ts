dee38208d70284b1272123930eea96fa14c1842a
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-04 01:53:32 +0200

    Update deliveries.ts

--- src/utils/deliveries.ts
+++ src/utils/deliveries.ts
@@ -1,9 +1,15 @@
 import * as State from '../state';
 
+/**
+ * getUndeliveredDeliveryIds. Method to get the ids of all undelivered deliveries.
+ * @param deliveries. A delivery array with all the driver deliveries.
+ * @param deliveredDeliveries A finished delivery array with all the deliveries delivered by the driver.
+ * @returns a string array with the ids of all the undelivered deliveries.
+ */
 const getUndeliveredDeliveryIds = (
   deliveries: State.Models.Delivery[],
   deliveredDeliveries: State.Models.FinishedDelivery[],
-) => {
+): string[] => {
   const deliveryIds: string[] = Array.from(deliveries, d => d.id);
   const deliveredDeliveryIds: string[] = Array.from(
     deliveredDeliveries,
@@ -12,10 +18,15 @@ const getUndeliveredDeliveryIds = (
   const undeliveredDeliveryIds = deliveryIds.filter(d =>
     deliveredDeliveryIds.includes(d),
   );
-
   return undeliveredDeliveryIds;
 };
 
+/**
+ * getUndeliveredDeliveries. Method to get all the undelivered driver deliveries.
+ * @param deliveries. A delivery array with all the driver deliveries.
+ * @param deliveredDeliveries A finished delivery array with all the deliveries delivered by the driver.
+ * @returns a delivery array with all the undelivered deliveries.
+ */
 const getUndeliveredDeliveries = (
   deliveries: State.Models.Delivery[],
   deliveredDeliveries: State.Models.FinishedDelivery[],
@@ -28,6 +39,12 @@ const getUndeliveredDeliveries = (
   );
 };
 
+/**
+ * getDeliveredDeliveries. Method to get all the delivered driver deliveries.
+ * @param deliveries. A delivery array with all the driver deliveries.
+ * @param deliveredDeliveries A finished delivery array with all the deliveries delivered by the driver.
+ * @returns a delivery array with all the delivered deliveries.
+ */
 const getDeliveredDeliveries = (
   deliveries: State.Models.Delivery[],
   deliveredDeliveries: State.Models.FinishedDelivery[],
@@ -37,10 +54,16 @@ const getDeliveredDeliveries = (
   );
 };
 
+/**
+ * isDelivered. Method to check if a delivery is already delivered.
+ * @param deliveredDeliveries A finished delivery array with all the deliveries delivered by the driver.
+ * @param deliveryId The id of the delivery to check.
+ * @returns a boolean representing if the delivery is delivered (true) or not (false).
+ */
 const isDelivered = (
   deliveredDeliveries: State.Models.FinishedDelivery[],
   deliveryId: string,
-) => {
+): boolean => {
   return (
     deliveredDeliveries.filter(dd => dd.deliveryId === deliveryId)?.length > 0
   );
