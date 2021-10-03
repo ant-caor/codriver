4ee18ba2923660939ecbaeeddd09acbbe509583c
Antonio Cabezas <contact@antoniocabezas.com>
2021-10-03 21:45:46 +0200

    Update activeDeliveryId.ts

--- src/state/atoms/activeDeliveryId.ts
+++ src/state/atoms/activeDeliveryId.ts
@@ -1,8 +1,10 @@
 import {atom} from 'recoil';
+import asyncStorageEffect from './effects/asyncStorageEffect';
 
 const activeDeliveryIdState = atom({
   key: 'activeDeliveryId',
   default: '',
+  effects_UNSTABLE: [asyncStorageEffect('activeDeliveryId')],
 });
 
 export default activeDeliveryIdState;
